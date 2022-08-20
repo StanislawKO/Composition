d090508880c55fc5d434b9ee7b8bc888b371ab92
Stanislav Kozh <printks@yandex.ru>
2022-08-20 18:04:54 +0300

    Change Serializable on Parcelable

--- app/src/main/java/com/codexample/composition/domain/entity/GameResult.kt
+++ app/src/main/java/com/codexample/composition/domain/entity/GameResult.kt
@@ -1,11 +1,13 @@
 package com.codexample.composition.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 
+@Parcelize
 data class GameResult (
-
     val winner: Boolean,
     val countOfRightAnswer: Int,
     val countOfQuestions: Int,
     val gameSettings: GameSettings
-): Serializable
+): Parcelable
--- app/src/main/java/com/codexample/composition/domain/entity/GameSettings.kt
+++ app/src/main/java/com/codexample/composition/domain/entity/GameSettings.kt
@@ -1,11 +1,14 @@
 package com.codexample.composition.domain.entity
 
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 
+@Parcelize
 data class GameSettings (
-
     val maxSumCount: Int,
     val minCountOfRightAnswer: Int,
     val minPercentOfRightAnswer: Int,
     val gameTimeInSeconds: Int
-): Serializable
+): Parcelable
